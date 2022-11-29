9775e94200e7c5f786a49b6a2e1790a2b9418589
Ryan Sheatsley <ryan@sheatsley.me>
2022-11-29 17:04:05 -0600

    rename templates to architectures and use __init__ to load all modules on package import

---
+++ sktorch/__init__.py
@@ -0,0 +1,7 @@
+"""
+This module initializes Scikit-Torch.
+Author: Ryan Sheatsley and Blaine Hoak
+Tue Nov 29 2022
+"""
+import sktorch.architectures  # hyperparameters and architecture bookkeeping
+import sktorch.models  # flexible pytorch-based models with scikit-learn-like interfaces
