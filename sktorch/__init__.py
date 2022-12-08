8e5a7da9ff9a6bc6bb3894025244d6508aa49812
Ryan Sheatsley <ryan@sheatsley.me>
2022-12-08 10:05:15 -0600

    migrate models module into init (no need for separate module)

--- sktorch/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This module initializes Scikit-Torch.
-Author: Ryan Sheatsley and Blaine Hoak
-Tue Nov 29 2022
-"""
-import sktorch.architectures  # hyperparameters and architecture bookkeeping
-import sktorch.models  # flexible pytorch-based models with scikit-learn-like interfaces
-import sktorch.utilities  # miscellaneous utility functions
