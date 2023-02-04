41b8ad1e7067a14cccf8b3b640c17ba3a800693a
Ryan Sheatsley <ryan@sheatsley.me>
2023-02-03 20:17:23 -0600

    add logic to set version as git head

--- dlm/__init__.py
+++ dlm/__init__.py
@@ -3,8 +3,16 @@ This module initializes the deep learning models repo.
 Author: Ryan Sheatsley and Blaine Hoak
 Tue Nov 29 2022
 """
+
+# import modules
 import dlm.architectures as architectures  # hyperparameters and architecture bookkeeping
 from dlm.models import LinearClassifier  # base class for all models
 from dlm.models import MLPClassifier  # feedforward deep neural network
 from dlm.models import CNNClassifier  # convolutional neural network
 import dlm.utilities as utilities  # miscellaneous utility functions
+import subprocess  # Subprocess management
+
+# compute version
+__version__ = subprocess.check_output(
+    ("git", "rev-parse", "--short", "HEAD"), text=True
+).strip()
