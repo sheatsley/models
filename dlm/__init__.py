ce7fbb54294e7df5aecc81addd04f1e5d6b5a884
Ryan Sheatsley <ryan@sheatsley.me>
2023-02-16 17:24:08 -0600

    pull imports into top level module

--- dlm/__init__.py
+++ dlm/__init__.py
@@ -5,11 +5,15 @@ Tue Nov 29 2022
 """
 
 # import modules
-import dlm.architectures as architectures  # hyperparameters and architecture bookkeeping
 from dlm.models import LinearClassifier  # base class for all models
 from dlm.models import MLPClassifier  # feedforward deep neural network
 from dlm.models import CNNClassifier  # convolutional neural network
-import dlm.utilities as utilities  # miscellaneous utility functions
+from dlm.templates import cicmalmem2022  # template for cicmalmem2022
+from dlm.templates import fmnist  # template for fmnist
+from dlm.templates import nslkdd  # template for nslkdd
+from dlm.templates import mnist  # template for mnist
+from dlm.templates import phishing  # template for phishing
+from dlm.templates import unswnb15  # template for unswnb15
 import subprocess  # Subprocess management
 
 # compute version
