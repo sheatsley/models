b66c949857a8ba62e0c324a3f9ab565bb37e16cb
Ryan Sheatsley <ryan@sheatsley.me>
2023-02-24 15:00:54 -0600

    convert all templates from namedtuples to dedicated classes (easy to encode dataset vars like number of classes)

--- dlm/templates.py
+++ dlm/templates.py
@@ -5,27 +5,26 @@ a form of bookkeeping to be passed as arguments to model instantiations.
 Author: Ryan Sheatsley and Blaine Hoak
 Thu Feb 2 2023
 """
-import collections  # Container datatypes
 import torch  # Tensors and Dynamic neural networks in Python with strong GPU acceleration
 
-# Templates are provided for linear models, mlps, cnns, & adversarial training
-Template = collections.namedtuple("Template", ("linear", "mlp", "cnn", "at"))
-"""
-CIC-MalMem-2022 (https://www.unb.ca/cic/datasets/malmem-2022.html) is for
-predicting malware categories and benign applciations. It has four labels that
-describe malware categories (trojan horse, spyware, ransomware, or benign). The
-dataset was designed to test obfuscated malware detection methods through
-memory dumps. The state-of-the-art MLP accuracy is 61%
-(https://pdfs.semanticscholar.org/b2e2/0dc7a34753311472a5f2314fbf866d7eddd0.pdf).
-"""
-cicmalmem2022 = Template(
-    None,
-    dict(
+
+class cicmalmem2022:
+    """
+    CIC-MalMem-2022 (https://www.unb.ca/cic/datasets/malmem-2022.html) is for
+    predicting malware categories and benign applciations. It has four labels
+    that describe malware categories (trojan horse, spyware, ransomware, or
+    benign). The dataset was designed to test obfuscated malware detection
+    methods through memory dumps. The state-of-the-art MLP accuracy is 61%
+    (https://pdfs.semanticscholar.org/b2e2/0dc7a34753311472a5f2314fbf866d7eddd0.pdf).
+    """
+
+    classes = 4
+    mlp = dict(
         activation=torch.nn.ReLU,
         attack=None,
         attack_params={},
         batch_size=128,
-        classes=4,
+        classes=classes,
         dropout=0.0,
         epochs=5,
         hidden_layers=(32,),
@@ -35,22 +34,24 @@ cicmalmem2022 = Template(
         optimizer_params={},
         scheduler=None,
         scheduler_params={},
-    ),
-    None,
-    None,
-)
-"""
-Fashion-MNIST (https://github.com/zalandoresearch/fashion-mnist) is a dataset
-for predicting Zalando's article images. It has ten labels that describe
-particular articles of clothing, encoded as "0" through "9" (i.e., t-shirt/top,
-trouser, pullover, dress, coat, sandal, shirt, sneaker, bag, ankle boot). The
-dataset was designed as a drop-in replacemnt for the original MNIST dataset for
-benchmarking machine learning algorithms. The state-of-the-art CNN accuracy is
-over 99% (https://arxiv.org/pdf/2001.00526.pdf).
-"""
-fmnist = Template(
-    None,
-    dict(
+    )
+
+
+class fmnist:
+    """
+    Fashion-MNIST (https://github.com/zalandoresearch/fashion-mnist) is a
+    dataset for predicting Zalando's article images. It has ten labels that
+    describe particular articles of clothing, encoded as "0" through "9" (i.e.,
+    t-shirt/top, trouser, pullover, dress, coat, sandal, shirt, sneaker, bag,
+    ankle boot). The dataset was designed as a drop-in replacemnt for the
+    original MNIST dataset for benchmarking machine learning algorithms. The
+    state-of-the-art CNN accuracy is over 99%
+    (https://arxiv.org/pdf/2001.00526.pdf).
+    """
+
+    classes = 10
+    shape = (1, 28, 28)
+    mlp = dict(
         activation=torch.nn.ReLU,
         attack=None,
         attack_params={},
@@ -65,8 +66,8 @@ fmnist = Template(
         optimizer_params={},
         scheduler=None,
         scheduler_params={},
-    ),
-    dict(
+    )
+    cnn = dict(
         activation=torch.nn.ReLU,
         attack=None,
         attack_params={},
@@ -83,25 +84,27 @@ fmnist = Template(
         optimizer_params={},
         scheduler=None,
         scheduler_params={},
-    ),
-    None,
-)
-"""
-The NSL-KDD (https://www.unb.ca/cic/datasets/nsl.html) contains extracted
-feature vectors from PCAPs that contain various information about traffic
-flows. It has five labels that describe benign traffic, denial-of-service
-attacks, network probes, user-to-root attacks, and remote-to-local attacks. The
-current state-of-the-art MLP accuracy is ~82%
-(https://www.ee.ryerson.ca/~bagheri/papers/cisda.pdf).
-"""
-nslkdd = Template(
-    None,
-    dict(
+        shape=shape,
+    )
+
+
+class nslkdd:
+    """
+    The NSL-KDD (https://www.unb.ca/cic/datasets/nsl.html) contains extracted
+    feature vectors from PCAPs that contain various information about traffic
+    flows. It has five labels that describe benign traffic, denial-of-service
+    attacks, network probes, user-to-root attacks, and remote-to-local attacks.
+    The current state-of-the-art MLP accuracy is ~82%
+    (https://www.ee.ryerson.ca/~bagheri/papers/cisda.pdf).
+    """
+
+    classes = 5
+    mlp = dict(
         activation=torch.nn.ReLU,
         attack=None,
         attack_params={},
         batch_size=128,
-        classes=5,
+        classes=classes,
         dropout=0.0,
         epochs=4,
         hidden_layers=(60, 32),
@@ -111,24 +114,25 @@ nslkdd = Template(
         optimizer_params={},
         scheduler=None,
         scheduler_params={},
-    ),
-    None,
-    None,
-)
-"""
-MNIST (http://yann.lecun.com/exdb/mnist/) is a dataset for predicting
-handwritten digits. It has ten labels that describe a particular digit, "0"
-through "9". The state-of-the-art CNN accuracy is over 99%
-(https://arxiv.org/pdf/1710.09829.pdf).
-"""
-mnist = Template(
-    None,
-    dict(
+    )
+
+
+class mnist:
+    """
+    MNIST (http://yann.lecun.com/exdb/mnist/) is a dataset for predicting
+    handwritten digits. It has ten labels that describe a particular digit, "0"
+    through "9". The state-of-the-art CNN accuracy is over 99%
+    (https://arxiv.org/pdf/1710.09829.pdf).
+    """
+
+    classes = 10
+    shape = (1, 28, 28)
+    mlp = dict(
         activation=torch.nn.ReLU,
         attack=None,
         attack_params={},
         batch_size=64,
-        classe=10,
+        classe=classes,
         dropout=0.0,
         hidden_layers=(512,),
         epochs=20,
@@ -138,13 +142,13 @@ mnist = Template(
         optimizer_params={},
         scheduler=None,
         scheduler_params={},
-    ),
-    dict(
+    )
+    cnn = dict(
         activation=torch.nn.ReLU,
         attack=None,
         attack_params={},
         batch_size=64,
-        classes=10,
+        classes=classes,
         conv_layers=(16, 32),
         dropout=0.4,
         epochs=20,
@@ -156,25 +160,27 @@ mnist = Template(
         optimizer_params={},
         scheduler=None,
         scheduler_params={},
-    ),
-    None,
-)
-"""
-The Phishing Dataset for Machine Learning: Feature Evaluation
-(https://www.fcsit.unimas.my/phishing-dataset) is a dataset for predicting
-whether a website is malicious based on features extracted from website DOMs
-and URLs. It has two classes, malicous and benign. The state-of-the-art MLP
-accuracy is 96%
-(https://www.sciencedirect.com/science/article/pii/S0020025519300763).
-"""
-phishing = Template(
-    None,
-    dict(
+        shape=shape,
+    )
+
+
+class phishing:
+    """
+    The Phishing Dataset for Machine Learning: Feature Evaluation
+    (https://www.fcsit.unimas.my/phishing-dataset) is a dataset for predicting
+    whether a website is malicious based on features extracted from website DOMs
+    and URLs. It has two classes, malicous and benign. The state-of-the-art MLP
+    accuracy is 96%
+    (https://www.sciencedirect.com/science/article/pii/S0020025519300763).
+    """
+
+    classes = 2
+    mlp = dict(
         activation=torch.nn.ReLU,
         attack=None,
         attack_params={},
         batch_size=32,
-        classes=2,
+        classes=classes,
         dropout=0.0,
         epochs=40,
         hidden_layers=(15,),
@@ -184,26 +190,26 @@ phishing = Template(
         optimizer_params={},
         scheduler=None,
         scheduler_params={},
-    ),
-    None,
-    None,
-)
-"""
-The UNSW-NB15 (https://research.unsw.edu.au/projects/unsw-nb15-dataset) is a
-dataset for predicting network intrusions from a blend of real benign traffic
-with sythetically generated attacks. It contains ten classes, with nine
-different attacks families and benign. The state-of-the-art MLP accuracy is 81%
-(https://www.sciencedirect.com/science/article/pii/S0957417419300843).
-"""
-unswnb15 = Template(
-    None,
-    None,
-    dict(
+    )
+
+
+class unswnb15:
+    """
+    The UNSW-NB15 (https://research.unsw.edu.au/projects/unsw-nb15-dataset) is
+    a dataset for predicting network intrusions from a blend of real benign
+    traffic with sythetically generated attacks. It contains ten classes, with
+    nine different attacks families and benign. The state-of-the-art MLP
+    accuracy is 81%
+    (https://www.sciencedirect.com/science/article/pii/S0957417419300843).
+    """
+
+    classes = 10
+    mlp = dict(
         activation=torch.nn.ReLU,
         attack=None,
         attack_params={},
         batch_size=128,
-        classes=10,
+        classes=classes,
         dropout=0.0,
         epochs=40,
         hidden_layers=(15,),
@@ -213,6 +219,4 @@ unswnb15 = Template(
         optimizer_params={},
         scheduler=None,
         scheduler_params={},
-    ),
-    None,
-)
+    )
