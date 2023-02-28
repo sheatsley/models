fc99fb7d46cf81c570cfeed20715cab28d010a98
Ryan Sheatsley <ryan@sheatsley.me>
2023-02-28 17:14:35 -0600

    rework imports with autoimport

--- dlm/__init__.py
+++ dlm/__init__.py
@@ -4,17 +4,22 @@ Author: Ryan Sheatsley and Blaine Hoak
 Tue Nov 29 2022
 """
 
-# import modules
-from dlm.models import LinearClassifier  # base class for all models
-from dlm.models import MLPClassifier  # feedforward deep neural network
-from dlm.models import CNNClassifier  # convolutional neural network
-from dlm.templates import cicmalmem2022  # template for cicmalmem2022
-from dlm.templates import fmnist  # template for fmnist
-from dlm.templates import nslkdd  # template for nslkdd
-from dlm.templates import mnist  # template for mnist
-from dlm.templates import phishing  # template for phishing
-from dlm.templates import unswnb15  # template for unswnb15
-import subprocess  # Subprocess management
+import subprocess
+
+from dlm.models import CNNClassifier, LinearClassifier, MLPClassifier
+from dlm.templates import cicmalmem2022, fmnist, mnist, nslkdd, phishing, unswnb15
+
+__all__ = [
+    "CNNClassifier",
+    "LinearClassifier",
+    "MLPClassifier",
+    "cicmalmem2022",
+    "fmnist",
+    "mnist",
+    "nslkdd",
+    "phishing",
+    "unswnb15",
+]
 
 # compute version
 __version__ = subprocess.check_output(
