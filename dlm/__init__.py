3e08a3cb374023662c86646e6bd07d7ec96d1078
Ryan Sheatsley <ryan@sheatsley.me>
2023-07-31 14:28:30 -0500

    adapt version computation to load static version file for non-dev installs

--- dlm/__init__.py
+++ dlm/__init__.py
@@ -1,12 +1,10 @@
 """
 This module initializes the deep learning models repo.
-Author: Ryan Sheatsley and Blaine Hoak
-Tue Nov 29 2022
 """
 
+import pathlib
 import subprocess
 
-import dlm.pretrained as pretrained
 import dlm.templates as templates
 from dlm.models import CNNClassifier, LinearClassifier, MLPClassifier
 
@@ -14,11 +12,12 @@ __all__ = [
     "CNNClassifier",
     "LinearClassifier",
     "MLPClassifier",
-    "pretrained",
     "templates",
 ]
-
-# compute version
-__version__ = subprocess.check_output(
-    ("git", "-C", *__path__, "rev-parse", "--short", "HEAD"), text=True
-).strip()
+try:
+    __version__ = subprocess.check_output(
+        ("git", "-C", *__path__, "rev-parse", "--short", "HEAD"), text=True
+    ).strip()
+except subprocess.CalledProcessError:
+    with open(pathlib.Path(__file__).parent / "VERSION", "r") as f:
+        __version__ = f.read().strip()
