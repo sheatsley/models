64d40c8a31524327d58cacb3ea846e5366282149
Ryan Sheatsley <ryan@sheatsley.me>
2023-07-31 14:23:46 -0500

    update version and add logic to embed version statically on non-dev installs

--- setup.py
+++ setup.py
@@ -1,9 +1,34 @@
 """
 Build script for Deep Learning Models
-Author: Ryan Sheatsley & Blaine Hoak
-Thu Feb 2 2023
 """
+import subprocess
+
 import setuptools
+import setuptools.command.install
+
+
+class Install(setuptools.command.install.install):
+    """
+    This class overrides the default install command so that the __version__
+    attribute of the dlm package is set statically.
+
+    :func:`run`: computes the git hash and saves it to a file
+    """
+
+    def run(self):
+        """
+        This method computes the git hash and saves it to a file.
+
+        :return: None
+        :rtype: Nonetype
+        """
+        version = subprocess.check_output(
+            ("git", "rev-parse", "--short", "HEAD"), text=True
+        ).strip()
+        with open("mlds/VERSION", "w") as f:
+            f.write(f"{version}\n")
+        return super().run()
+
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -11,11 +36,12 @@ with open("README.md", "r") as f:
 setuptools.setup(
     author="Ryan Sheatsley",
     author_email="ryan@sheatsley.me",
+    cmdclass=dict(install=Install),
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Scientific/Engineering :: Information Analysis",
     ],
@@ -27,7 +53,7 @@ setuptools.setup(
     keywords="machine-learning pytorch keras",
     name="dlm",
     packages=setuptools.find_packages(),
-    python_requires=">=3.10",
+    python_requires=">=3.11",
     url="https://github.com/sheatsley/models",
     version="1.1",
 )
